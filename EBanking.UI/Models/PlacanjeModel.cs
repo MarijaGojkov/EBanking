89c9ff655bfe36848de609a9c8abe4c7d6c769a0
Marija Gojkov <marija.gojkov2000@gmail.com>
2022-09-04 20:23:47 +0200

    dodata validacija za placanje

--- EBanking.UI/Models/PlacanjeModel.cs
+++ EBanking.UI/Models/PlacanjeModel.cs
@@ -4,11 +4,19 @@
     {
         public string BrojRacunaPlatioca { get; set; }
         public double TrenutnoStanje { get; set; }
-
         public string NazivPrimaoca { get; set; }
         public string BrojRacunaPrimaoca { get; set; }
         public string PozivNaBroj { get; set; }
         public string SvrhaPlacanja { get; set; }
         public double KolicinaNovca { get; set; }
+
+        #region Validacija
+        public string BrojRacunaPlatiocaError { get; set; }
+        public string NazivPrimaocaError { get; set; }
+        public string BrojRacunaPrimaocaError { get; set; }
+        public string PozivNaBrojError { get; set; }
+        public string SvrhaPlacanjaError { get; set; }
+        public string KolicinaNovcaError { get; set; }
+        #endregion
     }
 }
